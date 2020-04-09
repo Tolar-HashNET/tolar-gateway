347e4a81a344c77380a11103ef26c45d942676d5
Bartol Bilić <bartol.bilic1@gmail.com>
2020-04-09 12:54:48 +0200

    add more methods to account api

--- src/main/java/io/tolar/api/AccountApi.java
+++ src/main/java/io/tolar/api/AccountApi.java
@@ -1,13 +1,43 @@
 package io.tolar.api;
 
+import com.google.protobuf.ByteString;
+import com.googlecode.jsonrpc4j.JsonRpcMethod;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 import com.googlecode.jsonrpc4j.JsonRpcService;
+import io.tolar.proto.Account.AddressBalance;
 
+import java.util.List;
+
+//TODO check: JSON RPC always includes response field with key "result",
+// in Tolar docs some responses are mapped to other keys e.g. "json_key_file", "address"...
 @JsonRpcService("/account")
 public interface AccountApi {
+    @JsonRpcMethod("Create")
     boolean create(@JsonRpcParam(value = "master_password") String masterPassword);
 
+    @JsonRpcMethod("Open")
     boolean open(@JsonRpcParam(value = "master_password") String masterPassword);
 
+    @JsonRpcMethod("ListAddresses")
     byte[] listAddresses();
+
+    @JsonRpcMethod("VerifyAddress")
+    boolean verifyAddress(@JsonRpcParam(value = "address") ByteString address);
+
+    @JsonRpcMethod("CreateNewAddress")
+    ByteString createNewAddress(@JsonRpcParam(value = "name") String name,
+                                @JsonRpcParam(value = "lock_password") String lockPassword,
+                                @JsonRpcParam(value = "lock_hint") String hint);
+
+    @JsonRpcMethod("ExportKeyFile")
+    String exportKeyFile(@JsonRpcParam(value = "address") ByteString address);
+
+    @JsonRpcMethod("ImportKeyFile")
+    boolean importKeyFile(@JsonRpcParam(value = "json_key_file") String jsonKeyFile,
+                          @JsonRpcParam(value = "name") String name,
+                          @JsonRpcParam(value = "lock_password") String lockPassword,
+                          @JsonRpcParam(value = "lock_hint") String lockHint);
+
+    @JsonRpcMethod("ListBalancePerAddress")
+    List<AddressBalance> listBalancePerAddress();
 }
--- src/main/java/io/tolar/mock/AccountApiMock.java
+++ src/main/java/io/tolar/mock/AccountApiMock.java
@@ -1,8 +1,12 @@
 package io.tolar.mock;
 
+import com.google.protobuf.ByteString;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImpl;
 import io.tolar.api.AccountApi;
+import io.tolar.proto.Account;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @AutoJsonRpcServiceImpl
@@ -20,5 +24,30 @@ public class AccountApiMock implements AccountApi {
     @Override
     public byte[] listAddresses() {
         return new byte[0];
+    }
+
+    @Override
+    public boolean verifyAddress(ByteString address) {
+        return false;
+    }
+
+    @Override
+    public ByteString createNewAddress(String name, String lockPassword, String hint) {
+        return null;
+    }
+
+    @Override
+    public String exportKeyFile(ByteString address) {
+        return null;
+    }
+
+    @Override
+    public boolean importKeyFile(String jsonKeyFile, String name, String lockPassword, String lockHint) {
+        return false;
+    }
+
+    @Override
+    public List<Account.AddressBalance> listBalancePerAddress() {
+        return null;
     }
 }
