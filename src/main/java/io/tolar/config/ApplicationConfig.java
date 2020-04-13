d87e4a3a2e98d27a56d8de97918ca9c7c15fa93d
Smthng0 <Anonymus2304@gmail.com>
2020-04-13 13:14:12 +0200

    tidy pom, use undertow, spring version, add swagger

--- src/main/java/io/tolar/config/ApplicationConfig.java
+++ src/main/java/io/tolar/config/ApplicationConfig.java
@@ -3,12 +3,20 @@ package io.tolar.config;
 import com.googlecode.jsonrpc4j.spring.AutoJsonRpcServiceImplExporter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class ApplicationConfig {
+public class ApplicationConfig implements WebMvcConfigurer {
 
     @Bean
     public static AutoJsonRpcServiceImplExporter autoJsonRpcServiceImplExporter() {
         return new AutoJsonRpcServiceImplExporter();
     }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("", "/swagger");
+    }
+
 }
