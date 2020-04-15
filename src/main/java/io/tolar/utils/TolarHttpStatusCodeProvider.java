659641eb6aa606fdfda5049bea824d4f0b5ea1ed
Bartol Bilić <bartol.bilic1@gmail.com>
2020-04-15 16:14:17 +0200

    add 0 mapping

--- src/main/java/io/tolar/utils/TolarHttpStatusCodeProvider.java
+++ src/main/java/io/tolar/utils/TolarHttpStatusCodeProvider.java
@@ -6,6 +6,7 @@ public class TolarHttpStatusCodeProvider implements HttpStatusCodeProvider {
     @Override
     public int getHttpStatusCode(int i) {
         switch (i) {
+            case 0: return 200;
             case 1:
                 return 499;
             case 2:
