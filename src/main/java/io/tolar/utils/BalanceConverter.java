b5b29712bf4d4d02bf7f9315f6ccc895bedb5dbf
Smthng0 <Anonymus2304@gmail.com>
2020-04-22 15:25:55 +0200

    no need to ensure capacity

--- src/main/java/io/tolar/utils/BalanceConverter.java
+++ src/main/java/io/tolar/utils/BalanceConverter.java
@@ -1,17 +1,12 @@
 package io.tolar.utils;
 
-import com.google.common.primitives.Bytes;
 import com.google.protobuf.ByteString;
 
 import java.math.BigInteger;
 
 public class BalanceConverter {
-    private static final int MIN_LENGTH_OF_DOUBLE = 8;
 
     public static BigInteger convertBalance(ByteString balance) {
-        byte[] balanceInBytes = Bytes.ensureCapacity(balance.toByteArray(),
-                MIN_LENGTH_OF_DOUBLE, 0);
-
-        return new BigInteger(balanceInBytes);
+        return new BigInteger(balance.toByteArray());
     }
 }
