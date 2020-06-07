6c0ee3e70bfd5b2c3f345525902d69aef2e35025
Bartol Bilić <bartol.bilic1@gmail.com>
2020-06-07 20:51:24 +0200

    add eth_getBlockByNumber

--- src/main/java/io/tolar/api/EthereumApi.java
+++ src/main/java/io/tolar/api/EthereumApi.java
@@ -2,6 +2,7 @@ package io.tolar.api;
 
 import com.googlecode.jsonrpc4j.JsonRpcMethod;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
+import io.tolar.responses.EthBlock;
 
 public interface EthereumApi {
     @JsonRpcMethod("eth_getBalance")
@@ -9,4 +10,7 @@ public interface EthereumApi {
 
     @JsonRpcMethod("eth_blockNumber")
     String ethBlockNumber();
+
+    @JsonRpcMethod("eth_getBlockByNumber")
+    EthBlock ethGetBlockByNumber(@JsonRpcParam(value = "tag") String tag, @JsonRpcParam(value = "full_transaction") boolean isFullTransaction);
 }
