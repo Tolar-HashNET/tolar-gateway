50f5f239d16a1b9def2b6d2122a557eac23716e1
Bartol Bilić <bartol.bilic1@gmail.com>
2020-06-07 21:09:51 +0200

    fix

--- src/main/java/io/tolar/api/EthereumApiImpl.java
+++ src/main/java/io/tolar/api/EthereumApiImpl.java
@@ -46,7 +46,7 @@ public class EthereumApiImpl implements EthereumApi {
 
     @Override
     public String ethBlockNumber() {
-        BigInteger blockNumber = BigInteger.valueOf(tolarApi.getBlockCount()).min(BigInteger.ONE);
+        BigInteger blockNumber = BigInteger.valueOf(tolarApi.getBlockCount()).subtract(BigInteger.ONE);
         return Numeric.encodeQuantity(blockNumber);
     }
 
