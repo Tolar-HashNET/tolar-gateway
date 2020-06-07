87ec4a7eccd8b54f299ef14f296832a07e07dcb2
Bartol Bilić <bartol.bilic1@gmail.com>
2020-06-07 21:22:12 +0200

    mock impl of net_version and ethGetTransactionCount

--- src/main/java/io/tolar/api/EthereumApi.java
+++ src/main/java/io/tolar/api/EthereumApi.java
@@ -4,6 +4,8 @@ import com.googlecode.jsonrpc4j.JsonRpcMethod;
 import com.googlecode.jsonrpc4j.JsonRpcParam;
 import io.tolar.responses.EthBlock;
 
+import java.math.BigInteger;
+
 public interface EthereumApi {
     @JsonRpcMethod("eth_getBalance")
     String ethGetBalance(@JsonRpcParam(value = "address") String address, @JsonRpcParam(value = "tag") String tag);
@@ -13,4 +15,12 @@ public interface EthereumApi {
 
     @JsonRpcMethod("eth_getBlockByNumber")
     EthBlock ethGetBlockByNumber(@JsonRpcParam(value = "tag") String tag, @JsonRpcParam(value = "full_transaction") boolean isFullTransaction);
+
+    @JsonRpcMethod("net_version")
+    int netVersion();
+
+    @JsonRpcMethod("eth_getTransactionCount")
+    BigInteger ethGetTransactionCount(@JsonRpcParam(value = "address") String address, @JsonRpcParam(value = "tag") String tag);
 }
+
+
