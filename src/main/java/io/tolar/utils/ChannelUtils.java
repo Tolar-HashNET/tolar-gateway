62b44c20a88d5d71b4f72650d4b42c137df23886
fr33d00m <vice.simunov@gmail.com>
2020-06-15 17:07:30 +0200

    lets give it a go

--- src/main/java/io/tolar/utils/ChannelUtils.java
+++ src/main/java/io/tolar/utils/ChannelUtils.java
@@ -3,9 +3,11 @@ package io.tolar.utils;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.tolar.config.TolarConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-@Service
+@Configuration
 public class ChannelUtils {
     private TolarConfig tolarConfig;
 
@@ -13,6 +15,7 @@ public class ChannelUtils {
         this.tolarConfig = tolarConfig;
     }
 
+    @Bean
     public Channel getChannel() {
         return ManagedChannelBuilder
                 .forAddress(tolarConfig.getHost(), tolarConfig.getPort())
