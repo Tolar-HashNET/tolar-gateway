f0c796b4e7186b2e28aa043f102df69b345a68e3
fr33d00m <vice.simunov@gmail.com>
2020-06-15 17:59:55 +0200

    make a few channels

--- src/main/java/io/tolar/utils/ChannelUtils.java
+++ src/main/java/io/tolar/utils/ChannelUtils.java
@@ -3,23 +3,37 @@ package io.tolar.utils;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import io.tolar.config.TolarConfig;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
-@Configuration
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class ChannelUtils {
+    private static final int CHANNEL_NUMBER = 10;
     private TolarConfig tolarConfig;
+    private final List<Channel> channelList;
+    private final Random random;
 
     public ChannelUtils(TolarConfig tolarConfig) {
         this.tolarConfig = tolarConfig;
+        channelList = new ArrayList<>();
+        random = new Random();
+        for(int i = 0; i < CHANNEL_NUMBER; i++){
+            channelList.add(generateChannel());
+        }
+
     }
 
-    @Bean
-    public Channel getChannel() {
+    public Channel generateChannel(){
         return ManagedChannelBuilder
                 .forAddress(tolarConfig.getHost(), tolarConfig.getPort())
                 .usePlaintext()
                 .build();
+    }
+
+    public Channel getChannel() {
+        return channelList.get(random.nextInt(CHANNEL_NUMBER));
     }
 }
