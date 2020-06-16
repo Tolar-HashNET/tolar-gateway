4739e300c4715d9d276faa48aa476a976a3f02fe
fr33d00m <vice.simunov@gmail.com>
2020-06-16 07:57:30 +0200

    cache blocks

--- src/main/java/io/tolar/caching/NewTxCache.java
+++ src/main/java/io/tolar/caching/NewTxCache.java
@@ -3,32 +3,47 @@ package io.tolar.caching;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import org.springframework.stereotype.Service;
+import tolar.proto.Blockchain;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @Service
 public class NewTxCache {
-    private final Cache<String, String> txCache;
+    private final Cache<String, String> reverseTxCache;
+    private final Cache<Long, Blockchain.GetBlockResponse> blockCache;
 
     public NewTxCache(){
-        txCache = CacheBuilder.newBuilder()
+        reverseTxCache = CacheBuilder.newBuilder()
                 .maximumSize(10_000)
                 .expireAfterWrite(1, TimeUnit.MINUTES)
+                .build();
+
+        blockCache = CacheBuilder.newBuilder()
+                .maximumSize(10_000)
+                .expireAfterWrite(10, TimeUnit.MINUTES)
                 .build();
     }
 
     public boolean canProceed(String hash){
-        return ! txCache.asMap().containsKey(hash);
+        return ! reverseTxCache.asMap().containsKey(hash);
     }
 
     public void remove(List<String> hashes){
         for (String hash : hashes) {
-            txCache.invalidate(hash);
+            reverseTxCache.invalidate(hash);
         }
     }
 
+    public void put(Long blockNumber, Blockchain.GetBlockResponse block){
+        blockCache.put(blockNumber, block);
+    }
+
+    public Blockchain.GetBlockResponse getBlock(Long blockNumber){
+        return blockCache.getIfPresent(blockNumber);
+    }
+
     public void put(String txHash) {
-        txCache.put(txHash, txHash);
+        reverseTxCache.put(txHash, txHash);
     }
 }
