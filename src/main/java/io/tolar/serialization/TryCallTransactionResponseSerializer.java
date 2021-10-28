f68394cdfe194ae11f73cad2cbbb39a824aee9f4
josipmaricevic <33625854+josipmaricevic@users.noreply.github.com>
2021-10-28 14:38:00 +0200

    Fixes TryCall encoding result issue

--- src/main/java/io/tolar/serialization/TryCallTransactionResponseSerializer.java
+++ src/main/java/io/tolar/serialization/TryCallTransactionResponseSerializer.java
@@ -3,6 +3,7 @@ package io.tolar.serialization;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import org.bouncycastle.util.encoders.Hex;
 import tolar.proto.Blockchain;
 
 import java.io.IOException;
@@ -11,8 +12,10 @@ public class TryCallTransactionResponseSerializer extends JsonSerializer<Blockch
     @Override
     public void serialize(Blockchain.TryCallTransactionResponse tryCallTransactionResponse,
                           JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        String data = Hex.toHexString(tryCallTransactionResponse.getOutput().toByteArray());
+        
         jsonGenerator.writeStartObject();
-        jsonGenerator.writeObjectField("output", tryCallTransactionResponse.getOutput());
+        jsonGenerator.writeObjectField("output", data);
         jsonGenerator.writeObjectField("excepted", tryCallTransactionResponse.getExcepted());
         jsonGenerator.writeEndObject();
     }
